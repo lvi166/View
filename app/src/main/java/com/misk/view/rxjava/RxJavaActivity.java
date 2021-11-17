ed659e6a8e9b1ef3146f850c3489c702af34c419
lvjiajing@utry.cn <lvjiajing@utry.cn>
2021-11-17 18:02:49 +0800

    第一次提交

---
+++ app/src/main/java/com/misk/view/rxjava/RxJavaActivity.java
@@ -0,0 +1,19 @@
+package com.misk.view.rxjava;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * Create Time:  2021/11/17
+ * Author:  Administrator
+ * Description:
+ */
+public class RxJavaActivity  extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable  Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
